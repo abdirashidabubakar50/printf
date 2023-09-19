61ddf28bcd13ca44ee28564dd377b34c784121f2
Icelycoool <mohammedcali@proton.me>
2023-09-19 04:23:21 +0300

    Reverted to original status

--- _printf.c
+++ _printf.c
@@ -18,13 +18,12 @@ int _putchar(char c)
  */
 int _printf(const char *format, ...)
 {
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
-
 	char ch, *str,c;
 	int i, j, printed = 0;
 
 	va_list list;
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
 	va_start(list, format);
 	for (i = 0; format && format[i] != '\0'; i++)
 	{
